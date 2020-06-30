17c8f7e6c112907f28fc004af1e8c2f2d1d2f1e1
jarso <jaro7314@gmail.com>
2020-06-30 21:22:24 +0200

    creating matrix works ok

--- src/Galaktyka.java
+++ src/Galaktyka.java
@@ -2,87 +2,158 @@ public class Galaktyka {
 
     public static void main(String[] args) {
 
-        int telescopeSize, matrixWidth, matrixHeight, ind;
-        telescopeSize = 2;
+        int telescopeSize, matrixWidth, matrixHeight, lightYearsCounter;
+        int coordinateX, coordinateY;
+        String orientation;
+        int loopSize, numberOfStarsInRow;
+
+        // telescope size - first parameter
+        telescopeSize = 4;
+        // orientation - second parameter
+        orientation = "W";
+
+
+        // matrix size depends on telescope size
         matrixWidth = telescopeSize + 2;
         matrixHeight = telescopeSize + 3;
 
-//        String[][] matrixOfStars={
-//                {"*","*","*","*"},
-//                {".",".",".","*"},
-//                {"*","*",".","*"},
-//                {"*",".",".","*"},
-//                {"*","*","*","*"}
-//        };
 
         String[][] matrixOfStars = new String[matrixHeight][matrixWidth];
 
-        //dwie pierwsze linie są podobne w każdym wzorze
-        //zaczynamy w 3 od pozycji równej telescopeSize
-        //wykonujemy tyle pętli ile wynosi telescopeSize
-        //są 4 kierunki wypełniania - zrobić dzielenie wskażnika pętli modulo 4 (0,1,2,3)
-        //w zależności o wyniku modulo wypełniaj w dół, w lewo, w górę, w prawo
+        // a matrix is always created in the "W" orientation
+        // matrix is only printed according to orientation
+        // filling up whole matrix with blanks
+        for (int i = 0; i < matrixHeight; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
+                matrixOfStars[i][j] = ".";
+            }
+        }
 
-//        ind = 0;
-//
-//        for (int i = 0; i < matrixHeight; i++) {
-//            for (int j = 0; j < matrixWidth; j++) {
-//                matrixOfStars[i][j] = String.format("%-3s", Integer.toString(ind));
-//                ind++;
-//            }
-//        }
-
+        // filling up first row with stars
         for (int i = 0; i < matrixWidth; i++) {
             matrixOfStars[0][i] = "*";
         }
+        // there is always one star in second row
+        matrixOfStars[1][matrixWidth - 1] = "*";
 
-        //main loop, telescopeSize + 2
-        for (int i = 0; i < telescopeSize + 2; i++) {
+
+        System.out.println("telescope size: " + telescopeSize);
+
+
+        coordinateY = 0;
+        coordinateX = matrixWidth - 1;
+        loopSize = telescopeSize + 2;
+        numberOfStarsInRow = loopSize;
+
+        // main loop, length = telescopeSize + 2
+        for (int i = 0; i < loopSize; i++) {
+            if (i % 4 == 0) {
+                // drawing down
+                for (int j = 0; j < numberOfStarsInRow; j++) {
+                    coordinateY++;
+                    matrixOfStars[coordinateY][coordinateX] = "*";
+                }
+                numberOfStarsInRow--;
+
+
+            } else if (i % 4 == 1) {
+                // drawing left
+                for (int j = 0; j < numberOfStarsInRow; j++) {
+                    coordinateX--;
+                    matrixOfStars[coordinateY][coordinateX] = "*";
+                }
+                numberOfStarsInRow--;
+
+
+            } else if (i % 4 == 2) {
+                // drawing up
+                for (int j = 0; j < numberOfStarsInRow; j++) {
+                    coordinateY--;
+                    matrixOfStars[coordinateY][coordinateX] = "*";
+                }
+                numberOfStarsInRow--;
+
+
+            } else if (i % 4 == 3) {
+                // drawing right
+                for (int j = 0; j < numberOfStarsInRow; j++) {
+                    coordinateX++;
+                    matrixOfStars[coordinateY][coordinateX] = "*";
+                }
+                numberOfStarsInRow--;
+            }
 
         }
 
 
-        //printing in orientation
+        // printing in expected orientation
 
         //W
+        System.out.println("W");
+        printWestOrientation(matrixWidth, matrixHeight, matrixOfStars);
+        System.out.println();
+
+        //E
+        System.out.println("E");
+        printEastOrientation(matrixWidth, matrixHeight, matrixOfStars);
+        System.out.println();
+
+        //N
+        System.out.println("N");
+        printNorthOrientation(matrixWidth, matrixHeight, matrixOfStars);
+        System.out.println();
+
+        //S
+        System.out.println("S");
+        printSouthOrientation(matrixWidth, matrixHeight, matrixOfStars);
+        System.out.println();
+
+        // calculating light years
+        lightYearsCounter = 0;
+        for (int i = 0; i < matrixHeight; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
+                if (matrixOfStars[i][j].equals(".")) {
+                    lightYearsCounter++;
+                }
+            }
+        }
+        System.out.println("light years: " + lightYearsCounter);
+
+    }
+
+    private static void printWestOrientation(int matrixWidth, int matrixHeight, String[][] matrixOfStars) {
         for (int i = 0; i < matrixHeight; i++) {
             for (int j = 0; j < matrixWidth; j++) {
                 System.out.print(matrixOfStars[i][j]);
             }
             System.out.println();
         }
+    }
 
-        System.out.println();
-
-        //E
+    private static void printEastOrientation(int matrixWidth, int matrixHeight, String[][] matrixOfStars) {
         for (int i = matrixHeight - 1; i >= 0; i--) {
             for (int j = matrixWidth - 1; j >= 0; j--) {
                 System.out.print(matrixOfStars[i][j]);
             }
             System.out.println();
         }
+    }
 
-
-        System.out.println();
-
-        //N
+    private static void printNorthOrientation(int matrixWidth, int matrixHeight, String[][] matrixOfStars) {
         for (int j = 0; j < matrixWidth; j++) {
             for (int i = matrixHeight - 1; i >= 0; i--) {
                 System.out.print(matrixOfStars[i][j]);
             }
             System.out.println();
         }
+    }
 
-        System.out.println();
-
-        //S
+    private static void printSouthOrientation(int matrixWidth, int matrixHeight, String[][] matrixOfStars) {
         for (int j = matrixWidth - 1; j >= 0; j--) {
             for (int i = 0; i < matrixHeight; i++) {
                 System.out.print(matrixOfStars[i][j]);
             }
             System.out.println();
         }
-
-
     }
 }
